a733213660131a742f1fb91279d92d425a2d8bf6
Dave Richards <dadeece@gmail.com>
2017-10-29 21:55:48 +0000

    Slightly random snapshot of project - some outline GUI work complete

---
+++ Strikeometer-Upload/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Strikeometer_Upload
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        private readonly List<UploadDetails> _uploadQueue = new List<UploadDetails>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UploadGrid.ItemsSource = _uploadQueue;
+            UploadGrid.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Touch Name",
+                Binding = new Binding("TouchName")
+            });
+            UploadGrid.Columns.Add(new DataGridTextColumn
+            {
+                Header = "Touch URL",
+                Binding = new Binding("TouchURL")
+            });
+        }
+
+        private void AddFromComputer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Strikeometer-Upload/UploadDetails.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace Strikeometer_Upload
+{
+    public class UploadDetails : INotifyPropertyChanged
+    {
+        private string _touchName;
+        private string _touchURL;
+
+        public string TouchName
+        {
+            get => _touchName;
+            set
+            {
+                _touchName = value;
+                RaisePropertyChanged("TouchName");
+            }
+        }
+
+        public string TouchURL
+        {
+            get => _touchURL;
+            set
+            {
+                _touchURL = value;
+                RaisePropertyChanged("TouchURL");
+            }
+        }
+
+        public UploadDetails(string touchName, string touchUrl)
+        {
+            TouchName = touchName;
+            TouchURL = touchUrl;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
